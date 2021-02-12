5ca33d230675eae28efdc02b2d5273dd82715da0
Tsion-g <tsiongemechu6@gmail.com>
2021-02-12 20:51:55 +0000

    NestedLoop

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _mul - multiplies two integers
+ * @c: takes two int parameter
+ * Return: multiple of int integers
+ */
+int mul(int a, int b)
+{
+  return ( a * b );
+}
